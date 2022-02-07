86ceeaa45a82db78088655e5bf3ea6de67fd523f
jrayrcc <jray@rcconsulting.com>
2022-02-06 16:14:26 -0800

    first commit

---
+++ GeniusDocument_DebugLogging.h
@@ -0,0 +1,18 @@
+//
+//  GeniusDocument_DebugLogging.h
+//  Genius
+//
+//  Created by Chris Miner on 09.01.08.
+//  Copyright 2008 Chris Miner. All rights reserved.
+//
+//  Created by Jonathan Ray on 06.02.22.
+//  Copyright 2022 Jonathan Ray. All rights reserved.
+
+#import "GeniusDocument.h"
+
+@interface GeniusDocument(DebugLogging)
+
++ (void) installLogging;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,40 @@
+/*
+	Genius
+	Copyright (C) 2003-2006 John R Chang
+	Copyright (C) 2007-2008 Chris Miner
+    Copyright (C) 2022 Jonathan Ray
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.	
+
+	http://www.gnu.org/licenses/gpl.txt
+*/
+
+//  Genius
+//  Created by John R Chang on Sat Oct 18 2003.
+/*! @mainpage The Genius Project
+*
+* @section intro_sec Introductions
+*
+* Originally created by John R Chang on Sat Oct 18 2003.  Opensourced on Aug 27 2006.
+* Developed further by Chris Miner 2007-2008 under the GNU GPL.
+* Currently developed by Jonathan Ray 2022 under the GNU GPL.
+*
+* @section more_info For more information
+*
+* Visit the <a href="https://github.com/jrayrcc"> Jonathan Ray's gitbub</a> for this fork of the genius project
+*  
+*/
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char *argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
